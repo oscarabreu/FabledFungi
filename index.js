aba22a687260bc2048284d9acc60b120867889c9
Oscar Abreu <oja227@nyu.edu>
2023-11-23 15:56:14 -0500

    Fabled Fungis

--- index.js
+++ index.js
@@ -11,24 +11,23 @@ document.addEventListener("DOMContentLoaded", function () {
         imageElement.src = apiResponse.image_url;
 
     // Select HTML elements by class name and fill them with data
-    document.querySelector(".Taxon b").textContent = apiResponse.taxon_id;
     document.querySelector(".Mushroom-Name").textContent = apiResponse.taxon_species_name;
-    document.querySelector(".Common-Name b").textContent = apiResponse.common_name;
-    document.querySelector(".Observer b").textContent = apiResponse.observer_id;
     document.querySelector(".Observed b").textContent = apiResponse.observed_on;
-    document.querySelector(".User-ID b").textContent = apiResponse.user_id;
-    document.querySelector(".Description b").textContent = apiResponse.description;
+    document.querySelector(".Location b").textContent = apiResponse.place_guess;
     document.querySelector(".Latitude b").textContent = apiResponse.latitude;
     document.querySelector(".Longitude b").textContent = apiResponse.longitude;
-    document.querySelector(".Location b").textContent = apiResponse.place_guess;
     document.querySelector(".Kingdom-Name b").textContent = apiResponse.taxon_kingdom_name;
-    document.querySelector(".Order-Name b").textContent = apiResponse.taxon_order_name;
     document.querySelector(".Phylum-Name b").textContent = apiResponse.taxon_phylum_name;
-    document.querySelector(".Species-Name b").textContent = apiResponse.taxon_species_name;
+    document.querySelector(".Class-Name b").textContent = apiResponse.taxon_class_name;
+    document.querySelector(".Order-Name b").textContent = apiResponse.taxon_order_name;
     document.querySelector(".Family-Name b").textContent = apiResponse.taxon_family_name;
     document.querySelector(".Genus-Name b").textContent = apiResponse.taxon_genus_name;
+    document.querySelector(".Species-Name b").textContent = apiResponse.taxon_species_name;
+    const observer_id = apiResponse.observer_id.toString(); // Convert the ID to a string
+    document.querySelector(".source-link").href = `https://www.inaturalist.org/observations/${observer_id}`; // Set the href attribute
     document.querySelector(".User-Login b").textContent = apiResponse.user_login;
-    document.querySelector(".Source\\: b").textContent = apiResponse.url;
+    document.querySelector(".Description b").textContent = apiResponse.description;
+
 })
   .catch((error) => {
     console.error('Error loading data:', error); // Log an error message to the console
